29835d3f9698bc8964e79fa5566defb994411d4e
Kerry O'Connor <kerryoco@gmail.com>
2020-05-04 00:18:16 -0400

    up

--- client/js/world/ui/ACTION_BAR.js
+++ client/js/world/ui/ACTION_BAR.js
@@ -4,6 +4,9 @@ import POPUPS from './POPUPS.js'
 
 const action_bar = document.getElementById('action-bar')
 
+const char_display = ['name', 'height', 'speed']
+// const inv_display = []
+
 function init(){
 
 	init_action_buttons()
@@ -17,9 +20,59 @@ function init_character_buttons(){
 	const char_pop = new Popup({
 		id: 'character'
 	})
+	char_pop.render = function(){
+
+		for( const key of Object.keys( TOON )){
+
+			if( char_display.includes( key )){
+
+				let stat = document.createElement('div')
+				stat.classList.add('stat')
+				let stat_key = document.createElement('span')
+				stat_key.classList.add('stat-key')
+				stat_key.innerHTML = key
+				stat.appendChild( stat_key )
+				let stat_val = document.createElement('span')
+				stat_val.classList.add('stat-val')
+				stat_val.innerHTML = TOON[ key ]
+				stat.appendChild( stat_val )
+				
+				stat.appendChild( document.createElement('br'))
+				
+				char_pop.content.appendChild( stat )
+
+			}
+
+		}
+
+	}
 	const inv_pop = new Popup({
 		id: 'inventory'
 	})
+	inv_pop.render = function(){
+		
+		for( const mud_id of Object.keys( TOON.INVENTORY )){
+
+			let item = TOON.INVENTORY[ mud_id ]
+
+			let stat = document.createElement('div')
+			stat.classList.add('stat')
+			let stat_key = document.createElement('span')
+			stat_key.classList.add('stat-key')
+			stat_key.innerHTML = item.name
+			stat.appendChild( stat_key )
+			// let stat_val = document.createElement('span')
+			// stat_val.classList.add('stat-val')
+			// stat_val.innerHTML = TOON[ key ]
+			// stat.appendChild( stat_val )
+			
+			stat.appendChild( document.createElement('br'))
+			
+			inv_pop.content.appendChild( stat )
+
+		}
+
+	}
 
 	const character_wrapper = document.createElement('div')
 	character_wrapper.id = 'character-wrapper'
--- client/js/world/ui/Popup.js
+++ client/js/world/ui/Popup.js
@@ -1,80 +1,113 @@
-import POPUPS from './POPUPS.js'
-
+window.POPUPS = {};
+// function topbarElements(){
+    
+// }
 export default class Popup {
 	
-	constructor( init ){
+	constructor( options = {} ){
 
-		init = init || {}
-
-		if( !init.id ){
+        this.options = options;
+        
+		if( !options.id ){
 			console.log('cannot init popup without id')
 			return false
 		}
 
 		const ref = this
+        
+        this.w = 200;
+        this.h = 124;
+        this.topbarHeight = 16;
 
 		this.mousedown = false
 		this.lastX = 0
 		this.lastY = 0
 		this.curX = 100
-		this.curY = 50
+		this.curY = 0
 		this.diffX = 0
 		this.diffY = 0
+        
+        this.init()
 
-		this.element = document.createElement('div')
+		POPUPS[ options.id ] = this
+
+		document.body.appendChild( POPUPS[ options.id ].element )
+
+	}
+
+    init() {
+        this.initElements()
+        this.initEvents()
+    }
+    
+    initElements() {
+        this.element = document.createElement('div')
 		this.element.classList.add('popup')
-		this.element.id = init.id
-
-		this.title = document.createElement('div')
-		this.title.classList.add('title')
-		this.title.innerHTML = init.id
-		this.element.appendChild( this.title )
+		this.element.id = this.options.id
 
 		this.topbar = document.createElement('div')
 		this.topbar.classList.add('topbar')
-		this.topbar.addEventListener('mousedown', function(e){
-			ref.mousedown = true
-			ref.lastX = e.clientX
-			ref.lastY = e.clientY
-		})
-		this.topbar.addEventListener('mousemove', function(e){
-			if( ref.mousedown ){
-				ref.diffX = e.clientX - ref.lastX
-				ref.diffY = e.clientY - ref.lastY
-				ref.curX = ref.curX + ref.diffX
-				ref.curY = ref.curY + ref.diffY
-				ref.element.style.top = Math.floor( ref.curY ) + 'px'
-				ref.element.style.left = Math.floor( ref.curX ) + 'px'
-				ref.lastX = e.clientX
-				ref.lastY = e.clientY
-			}	
-		})
-		this.topbar.addEventListener('mouseup', function(e){
-			setTimeout(function(){
-				ref.mousedown = false
-			}, 10)
-		})
 		this.element.appendChild( this.topbar )
+        
+		this.title = document.createElement('div')
+		this.title.classList.add('title')
+		this.title.innerHTML = this.options.id
+		this.topbar.appendChild( this.title )
 
 		this.close = document.createElement('div')
 		this.close.classList.add('close')
 		this.close.innerHTML = 'X'
-		this.close.addEventListener('click', function(){
-			ref.element.style.display = 'none'
-		})
-		this.element.appendChild( this.close )
+		this.topbar.appendChild( this.close )
 
 		this.content = document.createElement('div')
 		this.content.classList.add('content')
 		this.element.appendChild( this.content )
+    }
 
-		POPUPS[ init.id ] = this
-
-		document.body.appendChild( POPUPS[ init.id ].element )
-
-	}
-
+    initEvents() {
+        const mousedown = e => {
+			this.mousedown = true
+			this.startX = e.clientX
+			this.startY = e.clientY
+			this.lastX = e.clientX
+			this.lastY = e.clientY
+            const rect = this.element.getBoundingClientRect();
+            this.left = rect.left;
+            this.top = rect.top;
+            this.diffX = this.startX - this.left
+            this.diffY = this.startY - this.top
+		};
+        const mousemove = e => {
+			if( this.mousedown ){
+				this.curX = e.clientX - this.diffX
+				this.curY = e.clientY - this.diffY
+				this.element.style.top = Math.floor( this.curY ) + 'px'
+				this.element.style.left = Math.floor( this.curX ) + 'px'
+				this.lastX = e.clientX
+				this.lastY = e.clientY
+			}	
+		};
+        const mouseup = e => {
+			this.mousedown = false
+		};
+        
+        this.topbar.addEventListener('mousedown', mousedown)
+		window.addEventListener('mousemove', mousemove)
+		window.addEventListener('mouseup', mouseup)
+        
+        this.topbar.addEventListener('touchstart', mousedown)
+		window.addEventListener('touchmove', mousemove)
+		window.addEventListener('touchend', mouseup)
+        
+        this.close.addEventListener('click', () => {
+			this.hide()
+		})
+    }
+    
 	show(){
+		
+		this.render()
+
 		this.element.style.display = 'inline-block'
 		this.element.style.left = this.curX
 		this.element.style.top = this.curY
@@ -82,6 +115,7 @@ export default class Popup {
 
 	hide(){
 		this.element.style.display = 'none'
+		this.content.innerHTML = ''
 	}
 
 	render(){
@@ -89,3 +123,5 @@ export default class Popup {
 	}
 
 }
+
+// const asdf = new Popup({id:"test"})
